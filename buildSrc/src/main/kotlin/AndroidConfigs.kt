e1f2dadfc7c616b6eb66a5cc09b137e7426896ea
ratobing <ratobing6@gmail.com>
2021-12-31 19:55:35 +0700

    downgrade compile sdk

--- buildSrc/src/main/kotlin/AndroidConfigs.kt
+++ buildSrc/src/main/kotlin/AndroidConfigs.kt
@@ -1,10 +1,10 @@
 object AndroidConfigs {
 
-    const val COMPILE_SDK   = 32
+    const val COMPILE_SDK   = 31
 
     object DefaultConfig {
         const val MIN_SDK       = 21
-        const val TARGET_SDK    = 32
+        const val TARGET_SDK    = 31
         const val VERSION_CODE  = 1
         const val VERSION_NAME  = "1.0"
 
