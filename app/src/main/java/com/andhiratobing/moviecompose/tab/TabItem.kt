6915a9cf2dab29f27cdf60a1f85bb2ef02aa7613
ratobing <ratobing6@gmail.com>
2022-01-02 05:23:34 +0700

    refactor import directory

--- app/src/main/java/com/andhiratobing/moviecompose/tab/TabItem.kt
+++ app/src/main/java/com/andhiratobing/moviecompose/tab/TabItem.kt
@@ -1,13 +1,15 @@
 package com.andhiratobing.moviecompose.tab
 
 import androidx.compose.runtime.Composable
-import com.andhiratobing.moviecompose.movie.MovieScreen
+import coil.annotation.ExperimentalCoilApi
+import com.andhiratobing.moviecompose.movie.movielist.presentation.MovieScreen
 import com.andhiratobing.moviecompose.tvshows.TvShowsScreen
 
 sealed class TabItem(
     var title: String,
     var screen: @Composable () -> Unit
 ) {
+    @ExperimentalCoilApi
     object Movie : TabItem(title = "Movie", screen = { MovieScreen() })
     object TvShows : TabItem(title = "TvShows", screen = { TvShowsScreen() })
 }
